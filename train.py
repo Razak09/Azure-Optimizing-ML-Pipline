6c93b4465c016cc7dd50f520f15dcaf4d90201a6
Razak09 <43496556+Razak09@users.noreply.github.com>
2020-10-31 18:14:58 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -18,20 +18,11 @@ path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-not
 
 ds = TabularDatasetFactory.from_delimited_files(path)
 
-x, y = clean_data(ds)
-
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
     jobs = pd.get_dummies(x_df.job, prefix="job")
@@ -50,9 +41,17 @@ def clean_data(data):
     x_df["month"] = x_df.month.map(months)
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df
     
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
